aacb99f0ea67e50d75a566444d49d53d7a902a28
Complex <166940834+complexwaremain@users.noreply.github.com>
2026-09-04 12:13:36 +0530

    Remove loading of universal.lua for VapeIndependent
    Removed conditional loading of universal.lua if VapeIndependent is not set.

--- main.lua
+++ main.lua
@@ -97,8 +97,6 @@ end
 vape = loadstring(downloadFile('newvape/guis/'..gui..'.lua'), 'gui')()
 shared.vape = vape
 
-if not shared.VapeIndependent then
-	loadstring(downloadFile('newvape/games/universal.lua'), 'universal')()
 	if isfile('newvape/games/'..game.PlaceId..'.lua') then
 		loadstring(readfile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
 	else
