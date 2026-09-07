4c40413df10dcca012db6a57c3fe867668ff86b5
Complex <166940834+complexwaremain@users.noreply.github.com>
2026-09-07 22:25:42 +0530

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -11,7 +11,7 @@ end
 local function downloadFile(path, func)
     if not isfile(path) then
         local suc, res = pcall(function()
-            return game:HttpGet('https://raw.githubusercontent.com/qyroke2/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+            return game:HttpGet('https://raw.githubusercontent.com/qyroke2/VapeV4ForRoblox/'..readfile('FlowVape/profiles/commit.txt')..'/'..select(1, path:gsub('FlowVape/', '')), true)
         end)
         if not suc or res == '404: Not Found' then
             error(res)
@@ -34,7 +34,7 @@ local function wipeFolder(path)
     end
 end
 
-for _, folder in {'newvape', 'newvape/games', 'newvape/profiles', 'newvape/assets', 'newvape/libraries', 'newvape/guis'} do
+for _, folder in {'FlowVape', 'FlowVape/games', 'FlowVape/profiles', 'FlowVape/assets', 'FlowVape/libraries', 'FlowVape/guis'} do
     if not isfolder(folder) then
         makefolder(folder)
     end
@@ -47,14 +47,14 @@ if not shared.VapeDeveloper then
     local commit = subbed:find('currentOid')
     commit = commit and subbed:sub(commit + 13, commit + 52) or nil
     commit = commit and #commit == 40 and commit or 'main'
-    if commit == 'main' or (isfile('newvape/profiles/commit.txt') and readfile('newvape/profiles/commit.txt') or '') ~= commit then
-        wipeFolder('newvape')
-        wipeFolder('newvape/games')
-        wipeFolder('newvape/guis')
-        wipeFolder('newvape/libraries')
-        wipeFolder('newvape/profiles') 
+    if commit == 'main' or (isfile('FlowVape/profiles/commit.txt') and readfile('FlowVape/profiles/commit.txt') or '') ~= commit then
+        wipeFolder('FlowVape')
+        wipeFolder('FlowVape/games')
+        wipeFolder('FlowVape/guis')
+        wipeFolder('FlowVape/libraries')
+        wipeFolder('FlowVape/profiles') 
     end
-    writefile('newvape/profiles/commit.txt', commit)
+    writefile('FlowVape/profiles/commit.txt', commit)
 end
 
 local BASE = "https://raw.githubusercontent.com/complexwaremain/FlowVape/main/profiles/"
@@ -75,7 +75,7 @@ for _, filename in ipairs(files) do
         return game:HttpGet(BASE .. filename)
     end)
     if ok and data and data ~= "404: Not Found" then
-        writefile("newvape/profiles/" .. filename, data)
+        writefile("FlowVape/profiles/" .. filename, data)
     end
 end
 
@@ -89,8 +89,8 @@ for _, filename in ipairs(gameFiles) do
         return game:HttpGet(GAMES_BASE .. filename)
     end)
     if ok and data and data ~= "404: Not Found" then
-        writefile("newvape/games/" .. filename, data)
+        writefile("FlowVape/games/" .. filename, data)
     end
 end
 
-return loadstring(downloadFile('newvape/main.lua'), 'main')()
+return loadstring(downloadFile('FlowVape/main.lua'), 'main')()
