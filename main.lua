bb141ecc3fd14e53a085ddd0a6fd8629b121046e
Complex <166940834+complexwaremain@users.noreply.github.com>
2026-09-08 15:42:38 +0530

    Update download URLs to new repository

--- main.lua
+++ main.lua
@@ -32,7 +32,7 @@ local playersService = cloneref(game:GetService('Players'))
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/qyroke2/VapeV4ForRoblox/'..readfile('FlowVape/profiles/commit.txt')..'/'..select(1, path:gsub('FlowVape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/complexwaremain/FlowVape/'..readfile('FlowVape/profiles/commit.txt')..'/'..select(1, path:gsub('FlowVape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
@@ -64,7 +64,7 @@ local function finishLoading()
 				if shared.VapeDeveloper then
 					loadstring(readfile('FlowVape/loader.lua'), 'loader')()
 				else
-					loadstring(game:HttpGet('https://raw.githubusercontent.com/qyroke2/VapeV4ForRoblox/'..readfile('FlowVape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
+					loadstring(game:HttpGet('https://raw.githubusercontent.com/complexwaremain/FlowVape/'..readfile('FlowVape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
 				end
 			]]
 			if shared.VapeDeveloper then
@@ -104,7 +104,7 @@ if not shared.VapeIndependent then
 	else
 		if not shared.VapeDeveloper then
 			local suc, res = pcall(function()
-				return game:HttpGet('https://raw.githubusercontent.com/qyroke2/VapeV4ForRoblox/'..readfile('FlowVape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
+				return game:HttpGet('https://raw.githubusercontent.com/complexwaremain/FlowVape/'..readfile('FlowVape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
 			end)
 			if suc and res ~= '404: Not Found' then
 				loadstring(downloadFile('FlowVape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
