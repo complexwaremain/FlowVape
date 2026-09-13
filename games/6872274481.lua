230e0de9f10b53cf7ec7b53c9cac77b00b04f1db
Complex <166940834+complexwaremain@users.noreply.github.com>
2026-09-13 17:42:45 +0530

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -11315,10 +11315,6 @@ run(function()
     })
 end)
 
-local oldcloneroot
-local cananticheatbypass = true
-local oldcloneroot
-local cananticheatbypass = true
 runFunction(function()
 local InfiniteFly = {Enabled = false}
 local InfiniteFlyMode = {Value = 'CFrame'}
